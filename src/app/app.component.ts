3473a878db1df1f92488ccffc62584f277a6d41e
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-17 17:15:21 +0300

    CSS added to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-class Item{
+class Item {
   purchase: string;
   done: boolean;
   price: number;
@@ -15,44 +15,55 @@ class Item{
 
 @Component({
   selector: 'my-app',
-  template: `<div class="page-header">
-        <h1> Список покупок </h1>
+  template: `
+    <div class="page-header">
+      <h1> Список покупок </h1>
     </div>
     <div class="panel">
-        <div class="form-inline">
-            <div class="form-group">
-                <div class="col-md-8">
-                    <input class="form-control" [(ngModel)]="text" placeholder = "Название" />
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-6">
-                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена" />
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
-                </div>
-            </div>
+      <div class="form-inline">
+        <div class="form-group">
+          <div class="col-md-8">
+            <input class="form-control" [(ngModel)]="text" placeholder="Название"/>
+          </div>
         </div>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Предмет</th>
-                    <th>Цена</th>
-                    <th>Куплено</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr *ngFor="let item of items">
-                    <td>{{item.purchase}}</td>
-                    <td>{{item.price}}</td>
-                    <td><input type="checkbox" [(ngModel)]="item.done" /></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>`
+        <div class="form-group">
+          <div class="col-md-6">
+            <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-md-offset-2 col-md-8">
+            <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
+          </div>
+        </div>
+      </div>
+      <table class="table table-striped">
+        <thead>
+        <tr>
+          <th>Предмет</th>
+          <th>Цена</th>
+          <th>Куплено</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr *ngFor="let item of items">
+          <td>{{item.purchase}}</td>
+          <td>{{item.price}}</td>
+          <td><input type="checkbox" [(ngModel)]="item.done"/></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>`,
+  styles: [`
+    h1, h2 {
+      color: green;
+    }
+
+    p {
+      font-size: 13px;
+      font-family: "Informal Roman", fantasy;
+    }
+  `]
 })
 export class AppComponent {
   text: string = "";
@@ -60,14 +71,16 @@ export class AppComponent {
 
   items: Item[] =
     [
-      { purchase: "Хлеб", done: false, price: 15.9 },
-      { purchase: "Масло", done: false, price: 60 },
-      { purchase: "Картофель", done: true, price: 22.6 },
-      { purchase: "Сыр", done: false, price:310 }
+      {purchase: "Хлеб", done: false, price: 15.9},
+      {purchase: "Масло", done: false, price: 60},
+      {purchase: "Картофель", done: true, price: 22.6},
+      {purchase: "Сыр", done: false, price: 310},
+      {purchase: "Прочая х**ня", done: false, price: 1500}
     ];
+
   addItem(text: string, price: number): void {
 
-    if(text==null || text.trim()=="" || price==null)
+    if (text == null || text.trim() == "" || price == null)
       return;
     this.items.push(new Item(text, price));
   }
